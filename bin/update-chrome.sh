8779fba219deac8a7271f0438404a3af23d85620
Ryan Binns <r@rbi.nz>
2022-05-01 02:22:23 +0800

    add bin

---
+++ bin/update-chrome.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get --only-upgrade install google-chrome-stable
---
+++ bin/update-vscode.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -O /tmp/code_latest_amd64.deb
+sudo dpkg -i /tmp/code_latest_amd64.deb
